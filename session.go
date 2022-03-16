3f55d1ec5ac1f95f64a6e0e22fafb9397b956e1a
austinlidey <alidey@ewu.edu>
2022-03-16 15:20:30 -0700

    Stub Session Functions

--- session.go
+++ session.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -18,4 +20,30 @@ type Session struct {
 func (session *Session) BeforeCreate(scope *gorm.DB) error {
 	session.ID = uuid.New().String()
 	return nil
+}
+
+/// 	CRUD Functions
+//	----------------------
+//Create a new session
+func SessionCreate(w http.ResponseWriter, r *http.Request) {
+	Create(w, r, &Session{})
+}
+
+//Get (read) an existing session
+func SessionGet(w http.ResponseWriter, r *http.Request) {
+	Get(w, r, &Session{}, func(db *gorm.DB) *gorm.DB { return db })
+}
+
+func SessionList(w http.ResponseWriter, r *http.Request) {
+	//Stub
+}
+
+//Update an existing session
+func SessionUpdate(w http.ResponseWriter, r *http.Request) {
+	Update(w, r, &Session{}, func(db *gorm.DB) *gorm.DB { return db })
+}
+
+//Delete an existing session
+func SessionDelete(w http.ResponseWriter, r *http.Request) {
+	Delete(w, r, &Session{})
 }
