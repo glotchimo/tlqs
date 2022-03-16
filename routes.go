5e004ff83945e257479a03e09963f12a915a6c5f
Elliott Maguire <glotchimo@pm.me>
2022-03-16 10:46:23 -0700

    add course CRUD stubs, tests, and routes (bad commit)

--- routes.go
+++ routes.go
@@ -9,13 +9,35 @@ type Route struct {
 	Handler http.HandlerFunc
 }
 
-// Add routes like this:
-// ```
-// {
-//     Name:    "Get Session",
-//     Method:  "GET",
-//     Path:    "/sessions/{id}",
-//     Handler: SessionGet
-// },
-// ```
-var Routes []Route = []Route{}
+var Routes []Route = []Route{
+	{
+		Name:    "Create Course",
+		Method:  http.MethodPost,
+		Path:    "/courses",
+		Handler: CourseCreate,
+	},
+	{
+		Name:    "Get Course",
+		Method:  http.MethodGet,
+		Path:    "/courses/{id}",
+		Handler: CourseGet,
+	},
+	{
+		Name:    "List Courses",
+		Method:  http.MethodGet,
+		Path:    "/courses",
+		Handler: CourseList,
+	},
+	{
+		Name:    "Update Course",
+		Method:  http.MethodPatch,
+		Path:    "/courses/{id}",
+		Handler: CourseUpdate,
+	},
+	{
+		Name:    "Delete Course",
+		Method:  http.MethodDelete,
+		Path:    "/courses/{id}",
+		Handler: CourseDelete,
+	},
+}
