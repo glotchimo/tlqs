2b2bf8af4dfd75d401a0c10adc70fe55a4d11413
Elliott Maguire <glotchimo@pm.me>
2022-03-16 10:46:23 -0700

    add course CRUD stubs, tests, and routes (bad commit)

--- course.go
+++ course.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -21,11 +23,58 @@ func (course *Course) BeforeCreate(scope *gorm.DB) error {
 
 func CourseCreate(w http.ResponseWriter, r *http.Request) {
 	var course Course
-
 	if err := json.NewDecoder(r.Body).Decode(&course); err != nil {
 		http.Error(w, "Couldn't decode request body.", http.StatusBadRequest)
 		return
 	}
 
 	Create(w, r, &course)
+}
+
+func CourseGet(w http.ResponseWriter, r *http.Request) {
+	Get(w, r, &Course{}, func(db *gorm.DB) *gorm.DB { return db })
+}
+
+func CourseList(w http.ResponseWriter, r *http.Request) {
+	page, err := strconv.Atoi(r.URL.Query().Get("offset"))
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal server error.", http.StatusInternalServerError)
+		return
+	} else if page == 0 {
+		page = 1
+	}
+
+	size, err := strconv.Atoi(r.URL.Query().Get("limit"))
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal server error.", http.StatusInternalServerError)
+		return
+	}
+
+	offset := (page - 1) * size
+
+	scope := func(db *gorm.DB) *gorm.DB {
+		if size == 0 {
+			return db.Offset(offset).Limit(size)
+		}
+
+		return db.Offset(offset)
+	}
+
+	List(w, r, &[]Course{}, scope)
+}
+
+func CourseUpdate(w http.ResponseWriter, r *http.Request) {
+	var course Course
+	if err := json.NewDecoder(r.Body).Decode(&course); err != nil {
+		http.Error(w, "Couldn't decode request body.", http.StatusBadRequest)
+		return
+	}
+
+	Update(w, r, &course, func(db *gorm.DB) *gorm.DB { return db })
+}
+
+func CourseDelete(w http.ResponseWriter, r *http.Request) {
+	Delete(w, r, &Course{})
 }
--- course_test.go
+++ course_test.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"bytes"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestCourseCreate(t *testing.T) {
-	body := []byte(`"title": "Machine Learning", "code": "CSCD496"`)
-
-	req := httptest.NewRequest("POST", "/courses", bytes.NewBuffer(body))
+	body := []byte(`{"title": "Machine Learning", "code": "CSCD496"}`)
+	req := httptest.NewRequest(http.MethodPost, "/courses", bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
 	rec := httptest.NewRecorder()
 
 	CourseCreate(rec, req)
@@ -17,5 +18,91 @@ func TestCourseCreate(t *testing.T) {
 
 	if res.StatusCode != 201 {
 		t.Errorf("expected status code 201, got %d", res.StatusCode)
+	}
+}
+
+func TestCourseGet(t *testing.T) {
+	c := Course{
+		Code:  "CSCD496",
+		Title: "Machine Learning",
+	}
+	result := Database.Create(&c)
+	if result.Error != nil {
+		t.Error(result.Error)
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/courses/"+c.ID, nil)
+	rec := httptest.NewRecorder()
+
+	CourseGet(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != 200 {
+		t.Errorf("expected status code 200, got %d", res.StatusCode)
+	}
+}
+
+func TestCourseList(t *testing.T) {
+	c := Course{
+		Code:  "CSCD496",
+		Title: "Machine Learning",
+	}
+	result := Database.Create(&c)
+	if result.Error != nil {
+		t.Error(result.Error)
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/courses?offset=10&limit=100", nil)
+	rec := httptest.NewRecorder()
+
+	CourseList(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != 200 {
+		t.Errorf("expected status code 200, got %d", res.StatusCode)
+	}
+}
+
+func TestCourseUpdate(t *testing.T) {
+	c := Course{
+		Code:  "CSCD260",
+		Title: "Architecture & Organization",
+	}
+	result := Database.Create(&c)
+	if result.Error != nil {
+		t.Error(result.Error)
+	}
+
+	body := []byte(`{"title": "Computer Architecture"}`)
+	req := httptest.NewRequest(http.MethodPatch, "/courses/"+c.ID, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	CourseUpdate(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != 200 {
+		t.Errorf("expected status code 200, got %d", res.StatusCode)
+	}
+}
+
+func TestCourseDelete(t *testing.T) {
+	c := Course{
+		Code:  "CSCD320",
+		Title: "Algorithms",
+	}
+	result := Database.Create(&c)
+	if result.Error != nil {
+		t.Error(result.Error)
+	}
+
+	req := httptest.NewRequest(http.MethodDelete, "/courses/"+c.ID, nil)
+	rec := httptest.NewRecorder()
+
+	CourseDelete(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != 200 {
+		t.Errorf("expected status code 200, got %d", res.StatusCode)
 	}
 }
