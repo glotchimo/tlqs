41fc8ddc119e943422f3b0b0570e4644337d1509
austinlidey <alidey@ewu.edu>
2022-03-17 11:09:55 -0700

    Stub Session Test

---
+++ session_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestSessionCreate(t *testing.T) {
+	//Stub
+}
+
+func TestSessionGet(t *testing.T) {
+	//Stub
+}
+
+func TestSessionList(t *testing.T) {
+	//Stub
+}
+
+func TestSessionUpdate(t *testing.T) {
+	//Stub
+}
+
+func TestSessionDelete(t *testing.T) {
+	//Stub
+}
