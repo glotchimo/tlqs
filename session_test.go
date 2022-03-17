d5d9fb4f4fbaf8cce8dc37683eee23fdd9c3745f
austinlidey <alidey@ewu.edu>
2022-03-17 11:27:13 -0700

    Write TestSessionCreate

--- session_test.go
+++ session_test.go
@@ -1,9 +1,24 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestSessionCreate(t *testing.T) {
-	//Stub
+	body := []byte(`{"student": "00123456", "tutor": "00654321", "course": "CSCD488", "topic": "Web Application Design", "description": "I'm having trouble implementing a web application, can you help me?", "retrospective": "Not sure what this is to be honest."}`)
+	req := httptest.NewRequest(http.MethodPost, "/sessions/", bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	SessionCreate(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != 201 {
+		t.Errorf("expected status code 201, got %d", res.StatusCode)
+	}
 }
 
 func TestSessionGet(t *testing.T) {
