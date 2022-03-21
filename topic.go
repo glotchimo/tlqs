202e072de997442a8326c5169cfea9ccfdcc1090
TechGlue <garcialuis24@hotmail.com>
2022-03-21 15:35:33 -0700

    added crud ops for topic. onto testing.

--- topic.go
+++ topic.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -14,4 +16,21 @@ type Topic struct {
 func (topic *Topic) BeforeCreate(scope *gorm.DB) error {
 	topic.ID = uuid.New().String()
 	return nil
+}
+
+func TopicCreate(w http.ResponseWriter, r *http.Request) {
+	//calling the generic create with the following struct.
+	Create(w, r, &Topic{})
+}
+
+func TopicGet(w http.ResponseWriter, r *http.Request) {
+	Get(w, r, &Topic{}, func(db *gorm.DB) *gorm.DB { return db })
+}
+
+func TopicUpdate(w http.ResponseWriter, r *http.Request) {
+	Update(w, r, &Course{}, func(db *gorm.DB) *gorm.DB { return db })
+}
+
+func CouseDelete(w http.ResponseWriter, r *http.Request) {
+	Delete(w, r, &Topic{})
 }
