12341d4d78ec7158ae54c1350a99b94234452be7
Elliott Maguire <glotchimo@pm.me>
2022-03-23 13:43:23 -0700

    add base auth0 ui wrappers

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -1,8 +1,15 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <App />,
+  <Auth0Provider
+    domain="dev-ewsga7nm.us.auth0.com"
+    clientId="4pMh2MZgYomKS287NIx3Jre4KN6H3ake"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById("root")
 );
