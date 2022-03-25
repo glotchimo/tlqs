3423b5c6e14e5c2507e1f1b6df9e3469ad2a4895
austinlidey <alidey@ewu.edu>
2022-03-24 17:18:59 -0700

    Change department choice page, add image assets

--- ui/src/Student/Test.jsx
+++ ui/src/Student/Test.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
+import './Test.css';
 import { Heading } from 'react-bulma-components';
+import mathDept from './img/math_dept.svg';
+import csDept from './img/cs_dept.svg';
+
 
 
 function Test() {
     return (
-        <>
-            <Heading>
-                Title
-            </Heading>
-            <Heading subtitle>
-                Subtitle
-            </Heading>
-        </>
+        <div class="columns">
+            <div class="column is-half" id="col-left">
+                <figure class="image is-square" id="math_img">
+                    <img src={mathDept}></img>
+                </figure>
+                {/* Left column */}
+            </div>
+            <div class="column is-half" id="col-right">
+                <figure class="image is-square" id="cs_img">
+                    <img src={csDept}></img>
+                </figure>
+                {/* Right column */}
+            </div>
+        </div>
+
     );
 }
 
--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -5,13 +5,14 @@ import Test from "./Student/Test";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-ewsga7nm.us.auth0.com"
-    clientId="4pMh2MZgYomKS287NIx3Jre4KN6H3ake"
-    redirectUri={window.location.origin}
-  >
-    <App />
-    <Test />
-  </Auth0Provider>,
+  // <Auth0Provider
+  //   domain="dev-ewsga7nm.us.auth0.com"
+  //   clientId="4pMh2MZgYomKS287NIx3Jre4KN6H3ake"
+  //   redirectUri={window.location.origin}
+  // >
+  //   <App />
+
+  // </Auth0Provider>,
+  <Test />,
   document.getElementById("root")
 );
