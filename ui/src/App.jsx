81c3f32b3632659e418efae6a128792528ec4531
TechGlue <garcialuis24@hotmail.com>
2022-04-10 03:47:37 -0700

    comment explaining the purpose of this branch

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,3 +1,5 @@
+//Was not able to find api route today. So utilized a test api to figure out how I would approach fetching and displaying data. 
+
 import * as React from "react";
 import TutorMain from "./tutor/TutorMain";
 export default () => {
