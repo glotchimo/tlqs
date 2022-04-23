605bccde0e5f740bc3a6478d7144cd8e52699733
Elliott Maguire <glotchimo@pm.me>
2022-04-23 15:02:47 -0700

    add entryway auth router

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,18 +1,55 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import * as React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import * as React from "react";
+import Admin from "./views/Admin/Admin.jsx";
+import Student from "./views/Student/Student.jsx";
+import Tutor from "./views/Tutor/Tutor.jsx";
 
 export default () => {
-    const { isLoading, isAuthenticated } = useAuth0();
+  const {
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    user,
+    getAccessTokenSilently,
+  } = useAuth0();
 
-    return (
-        <>
-            {isLoading && "Loading"}
-            {!isLoading && (
-                <>
-                    {!isAuthenticated && "Reroute to SSO"}
-                    {isAuthenticated && "Route to whatever view"}
-                </>
-            )}
-        </>
-    );
+  const [role, setRole] = React.useState(-1);
+
+  React.useEffect(() => {
+    if (user == null) {
+      return;
+    }
+
+    fetch("/users/", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Authentication: `Bearer ${getAccessTokenSilently().then((t) => t)}`,
+      },
+      body: JSON.stringify({ name: user.name, email: user.email }),
+    })
+      .then((response) => response.json())
+      .then((data) => setRole(data.role))
+      .catch((e) => console.log(e));
+  }, [user]);
+
+  if (isLoading) {
+    return "Loading...";
+  }
+
+  if (isAuthenticated) {
+    switch (role) {
+      case 0:
+        return <Student />;
+      case 1:
+        return <Tutor />;
+      case 2:
+        return <Admin />;
+      default:
+        return "Loading...";
+    }
+  } else {
+    return loginWithRedirect();
+  }
 };
