3a92b28956334e240f23668587506f4ba1c7f254
Elliott Maguire <glotchimo@pm.me>
2022-04-23 15:02:29 -0700

    update index entrypoint

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-ewsga7nm.us.auth0.com"
+    clientId="4pMh2MZgYomKS287NIx3Jre4KN6H3ake"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+);
