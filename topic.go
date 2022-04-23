fa4809aa37233a56056b7f9533840dc52dadd79d
Elliott Maguire <glotchimo@pm.me>
2022-04-22 18:05:02 -0700

    remove old offset scope code and update to new List prototype

--- topic.go
+++ topic.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -28,50 +26,14 @@ func TopicGet(w http.ResponseWriter, r *http.Request) {
 	Get(w, r, &Topic{}, func(db *gorm.DB) *gorm.DB { return db })
 }
 
+func TopicList(w http.ResponseWriter, r *http.Request) {
+	List(w, r, &[]Topic{}, ByOffset(w, r), ByCourse(w, r))
+}
+
 func TopicUpdate(w http.ResponseWriter, r *http.Request) {
 	Update(w, r, &Topic{}, func(db *gorm.DB) *gorm.DB { return db })
 }
 
 func TopicDelete(w http.ResponseWriter, r *http.Request) {
 	Delete(w, r, &Topic{})
-}
-
-func TopicList(w http.ResponseWriter, r *http.Request) {
-	strOffset := r.URL.Query().Get("offset")
-	if strOffset == "" {
-		strOffset = "0"
-	}
-
-	strLimit := r.URL.Query().Get("limit")
-	if strLimit == "" {
-		strLimit = "100"
-	}
-
-	page, err := strconv.Atoi(strOffset)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Internal server error.", http.StatusInternalServerError)
-		return
-	} else if page == 0 {
-		page = 1
-	}
-
-	size, err := strconv.Atoi(strLimit)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Internal server error.", http.StatusInternalServerError)
-		return
-	}
-
-	offset := (page - 1) * size
-
-	scope := func(db *gorm.DB) *gorm.DB {
-		if size == 0 {
-			return db.Offset(offset).Limit(size)
-		}
-
-		return db.Offset(offset)
-	}
-
-	List(w, r, &[]Topic{}, scope)
 }
