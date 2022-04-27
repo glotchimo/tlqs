f596a5370c0f362fddbb175838d941176c6c8099
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-27 14:27:03 -0700

    adding data grid imports

--- ui/src/views/Admin/Admin.jsx
+++ ui/src/views/Admin/Admin.jsx
@@ -1,5 +1,16 @@
 import * as React from "react";
+import React, { useState, useEffect } from "react";
+import { DataGrid} from '@mui/x-data-grid';
 
+const columns = [
+  { field: 'name', headerName: 'Names', width: 130 },
+  { field: 'email', headerName: 'Email', width: 130 },
+  { field: 'role', headerName: 'Role', width: 130 },
+  { field: 'sessions', headerName: 'Sessions', width: 130 },
+  { field: 'courses', headerName: 'Courses', width: 130 }, 
+];
 export default () => {
+
+  
   return "Admin";
 };
