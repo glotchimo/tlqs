b9cb5c3dc3e8f74d1d0d3a08604c45ea5ab004b2
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-27 22:45:19 -0700

    course skeleton

---
+++ ui/src/views/Admin/Courses.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import React, { useState, useEffect } from "react";
+import { DataGrid} from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'title', headerName: 'Ttile', width: 130 },
+  { field: 'code', headerName: 'Code', width: 130 },
+  
+];
+export default function Courses() {
+  
+}
