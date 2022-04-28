81c1121b9592eb86968eab7f1533779e5337f73a
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-27 22:41:38 -0700

    finishing session

--- ui/src/views/Admin/Sessions.jsx
+++ ui/src/views/Admin/Sessions.jsx
@@ -14,5 +14,33 @@ const columns = [
   
 ];
 export default ()=> {
-
+    const [rows, setRows] = useState([]);
+    const apiGet = () => {
+      fetch("http://localhost:8080/sessions/")
+        .then((response) => response.json())
+        .then((json) => {
+          setRows(json);
+            json.map((session)=>{
+  
+              rows.push({student:session.student, tutor:session.tutor, course: session.course, topic:session.topic, description:session.description,retrospective:session.retrospective},);
+            })
+        });
+    };
+  
+    useEffect(() => {
+      apiGet();
+    }, []);
+  
+    return (
+      
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+         rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
+    );
 }
