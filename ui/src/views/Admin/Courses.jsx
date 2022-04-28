6572da0bb6f85ab19bdbe29820aad75a4e787f36
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-27 22:46:29 -0700

    formatting

--- ui/src/views/Admin/Courses.jsx
+++ ui/src/views/Admin/Courses.jsx
@@ -4,9 +4,28 @@ import { DataGrid} from '@mui/x-data-grid';
 
 const columns = [
   { field: 'title', headerName: 'Ttile', width: 130 },
-  { field: 'code', headerName: 'Code', width: 130 },
-  
-];
+  { field: 'code', headerName: 'Code', width: 130 },];
 export default function Courses() {
-  
-}
+    const [rows, setRows] = useState([]);
+  const apiGet = () => {
+    fetch("/courses/")
+      .then((response) => response.json())
+      .then((json) => {
+        setRows(json);
+          json.map((course)=>{
+            rows.push({title:course.title, code:course.code},);
+          })}); };
+  useEffect(() => {
+    apiGet();
+  }, []);
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+       rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );}
