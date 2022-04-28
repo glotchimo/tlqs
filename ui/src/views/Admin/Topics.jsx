7d95e3f49fb214e5a42874fae69ee4628a5501db
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-27 22:57:17 -0700

    finishing the rest of the file

--- ui/src/views/Admin/Topics.jsx
+++ ui/src/views/Admin/Topics.jsx
@@ -4,9 +4,28 @@ import { DataGrid} from '@mui/x-data-grid';
 
 const columns = [
   { field: 'course', headerName: 'CourseID', width: 130 },
-  { field: 'name', headerName: 'Name', width: 130 },
-  
-];
+  { field: 'name', headerName: 'Name', width: 130 },];
 export default ()=> {
-
-}
+    const [rows, setRows] = useState([]);
+    const apiGet = () => {
+      fetch("http://localhost:8080/topics/")
+        .then((response) => response.json())
+        .then((json) => {
+          setRows(json);
+            json.map((topic)=>{
+              rows.push({course:topic.course,  name:topic.name},);
+            })});};
+    useEffect(() => {
+      apiGet();
+    }, []);
+    return (
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+         rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
+    );}
