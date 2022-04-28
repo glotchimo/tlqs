aab47e9af926f4cc85c8c655c28341b51b3f7bc6
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-27 22:56:12 -0700

    topic skeleton

---
+++ ui/src/views/Admin/Topics.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import React, { useState, useEffect } from "react";
+import { DataGrid} from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'course', headerName: 'CourseID', width: 130 },
+  { field: 'name', headerName: 'Name', width: 130 },
+  
+];
+export default ()=> {
+
+}
