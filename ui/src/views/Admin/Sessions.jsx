d2c30172ef9e2f235a89cd71d78413a08aeb879b
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-27 22:41:08 -0700

    creating skeleton session

---
+++ ui/src/views/Admin/Sessions.jsx
@@ -0,0 +1,18 @@
+
+  
+import * as React from 'react';
+import React, { useState, useEffect } from "react";
+import { DataGrid} from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'student', headerName: 'Students', width: 120 },
+  { field: 'tutor', headerName: 'Tutors', width: 120 },
+  { field: 'course', headerName: 'Courses', width: 120 },
+  { field: 'topic', headerName: 'Topics', width: 120 },
+  { field: 'description', headerName: 'Descreiptions', width: 350 },
+  { field: 'retrospective', headerName: 'Retrospective', width: 400 },
+  
+];
+export default ()=> {
+
+}
