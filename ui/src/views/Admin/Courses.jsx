ed4355cb58a58b41d3e791b5a1ea80cec012a6bc
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-28 18:36:03 -0700

    adding dependencies

--- ui/src/views/Admin/Courses.jsx
+++ ui/src/views/Admin/Courses.jsx
@@ -1,12 +1,43 @@
 import * as React from 'react';
 import React, { useState, useEffect } from "react";
 import { DataGrid} from '@mui/x-data-grid';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import React,{ useState } from "react";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-const columns = [
-  { field: 'title', headerName: 'Ttile', width: 130 },
-  { field: 'code', headerName: 'Code', width: 130 },];
-export default function Courses() {
-    const [rows, setRows] = useState([]);
+
+export default ()=> {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 130 },
+    { field: 'title', headerName: 'Ttile', width: 130 },
+    { field: 'code', headerName: 'Code', width: 130 },
+    {field: 'delete', headerName: 'Delete', width: 100,
+    renderCell: (params) => {
+      return (
+        <DeleteOutlineIcon onClick={() => handleDelete(params.row.id)}/>)},},
+  
+    {field: 'edit', headerName: 'Edit', width: 100,
+    renderCell: (params) => {
+      return (
+        // a dom LINK here in a tag to link it to apage where user can edit
+            <> <EditIcon /></>
+        )},
+    },
+    {field: 'reate', headerName: 'Create', width: 100,
+    renderCell: (params) => {
+      return (
+        // a dom here to link to a page like <Link to {page we want}<Link> to create a new user
+        <> <AddCircleIcon /></>)},
+    },];
+  
+  const [rows, setRows] = useState([]);
   const apiGet = () => {
     fetch("/courses/")
       .then((response) => response.json())
