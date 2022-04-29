1619c5ac54c9bb67c9635930c551ccf9ea17b8d4
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-28 18:46:42 -0700

    creating colums

--- ui/src/views/Admin/Topics.jsx
+++ ui/src/views/Admin/Topics.jsx
@@ -2,10 +2,30 @@ import * as React from 'react';
 import React, { useState, useEffect } from "react";
 import { DataGrid} from '@mui/x-data-grid';
 
-const columns = [
-  { field: 'course', headerName: 'CourseID', width: 130 },
-  { field: 'name', headerName: 'Name', width: 130 },];
 export default ()=> {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 130 },
+    { field: 'course', headerName: 'CourseID', width: 130 },
+    { field: 'name', headerName: 'Name', width: 130 },
+    {field: 'delete', headerName: 'Delete', width: 100,
+    renderCell: (params) => {
+      return (
+        <DeleteOutlineIcon onClick={() => handleDelete(params.row.id)}/>)},},
+  
+    {field: 'edit', headerName: 'Edit', width: 100,
+    renderCell: (params) => {
+      return (
+        // a dom LINK here in a tag to link it to apage where user can edit
+            <> <EditIcon /></>
+        )},
+    },
+    {field: 'reate', headerName: 'Create', width: 100,
+    renderCell: (params) => {
+      return (
+        // a dom here to link to a page like <Link to {page we want}<Link> to create a new user
+        <> <AddCircleIcon /></>)},
+    },];
+
     const [rows, setRows] = useState([]);
     const apiGet = () => {
       fetch("http://localhost:8080/topics/")
@@ -15,9 +35,15 @@ export default ()=> {
             json.map((topic)=>{
               rows.push({course:topic.course,  name:topic.name},);
             })});};
+
     useEffect(() => {
       apiGet();
     }, []);
+    
+    const handleDelete = (id) => {
+      setRows(rows.filter((item) => item.id !== id));
+    };
+
     return (
       <div style={{ height: 400, width: '100%' }}>
         <DataGrid
