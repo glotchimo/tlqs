c95a9f25f835f221f4893609f6b2a8ea5f668ec1
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-28 18:20:51 -0700

    adding icons for delete,edit create

--- ui/src/views/Admin/Admin.jsx
+++ ui/src/views/Admin/Admin.jsx
@@ -8,33 +8,59 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import React,{ useState } from "react";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-const columns = [
-  { field: 'name', headerName: 'Names', width: 200},
-  { field: 'email', headerName: 'Email', width: 200 },
-  { field: 'role', headerName: 'Role', width: 80},
-  { field: 'sessions', headerName: 'Sessions', width: 200 },
-  { field: 'courses', headerName: 'Courses', width: 200,},
-  {field: 'delete', headerName: 'Delete', width: 100, },
-  {field: 'edit', headerName: 'Edit', width: 100},
-  {field: 'reate', headerName: 'Edit', width: 100},
 
-];
 export default () =>{
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 200},
+    { field: 'name', headerName: 'Names', width: 200},
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'role', headerName: 'Role', width: 80},
+    { field: 'sessions', headerName: 'Sessions', width: 200 },
+    { field: 'courses', headerName: 'Courses', width: 200,},
+    {field: 'delete', headerName: 'Delete', width: 100,
+    renderCell: (params) => {
+      return (
+        <DeleteOutlineIcon onClick={() => handleDelete(params.row.id)}/>)},},
+  
+    {field: 'edit', headerName: 'Edit', width: 100,
+    renderCell: (params) => {
+      return (
+        // a dom LINK here in a tag to link it to apage where user can edit
+            <> <EditIcon /></>
+        )},
+    },
+    {field: 'reate', headerName: 'Create', width: 100,
+    renderCell: (params) => {
+      return (
+        // a dom here to link to a page like <Link to {page we want}<Link> to create a new user
+        <> <AddCircleIcon /></>)},
+    },
+  ];
+
   const [rows, setRows] = useState([]);
+  useEffect(() => {
+    apiGet();
+  }, []);
+
+  const handleDelete = (id) => {
+    setRows(rows.filter((item) => item.id !== id));
+  };
+
   const apiGet = () => {
     fetch("http://localhost:8080/users/")
       .then((response) => response.json())
       .then((json) => {
         setRows(json);
           json.map((user)=>{
-            rows.push({name:user.name, email: user.email, role:user.role, 
+            rows.push({id:user.id,name:user.name, email: user.email, role:user.role, 
               sessions: user.sessions, courses:user.courses,
             },);
           })});};
-  useEffect(() => {
-    apiGet();
-  }, []);
+
   return (
     <div style={{ height: 400, width: '100%' }}>
         <Box sx={{ flexGrow: 1 }}>
@@ -59,22 +85,5 @@ export default () =>{
         checkboxSelection
       />
     </div>
-  );}
-
-/*
-  renderCell: (params) => {
-    return (
-      <>
-        <Link to={"/user/" + params.row.id}>
-          <button className="userListEdit">Edit</button>
-        </Link>
-        <DeleteOutline
-          className="userListDelete"
-          onClick={() => handleDelete(params.row.id)}
-        />
-      </>
-
-      <delete className="delete"><Button variant="contained">Delete</Button></delete>
-
-      
-    );},*/
+  );
+}
