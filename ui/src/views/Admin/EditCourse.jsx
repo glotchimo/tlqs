4c8f9eb7cd5890fe70be80130b16433d5aba920d
hewrtarkhany <hewrtarkhany@gmail.com>
2022-04-30 14:47:55 -0700

    edit course jsx file fully working

---
+++ ui/src/views/Admin/EditCourse.jsx
@@ -0,0 +1,46 @@
+import "./editcourse.css";
+export default  function CourseEdit() {
+
+return (
+<div className="user">
+  <div className="titleContainer">
+    <h1 className="title">Edit Course</h1>
+  </div>
+  <div className="container">
+    <div className="update">
+      <form className="form">
+        <div className="left">
+
+          <div className="item">
+
+            <label>Title</label>
+            <input type="text" placeholder="title" className="input" />
+          </div>
+
+          <div className="item">
+            <label>Department</label>
+            <input type="text" placeholder="department" className="input" />
+          </div>
+
+          <div className="item">
+            <label>Code</label>
+            <input type="text" placeholder="code" className="input" />
+          </div>
+
+        </div>
+        <div className="right">
+          <div className="upload">
+            <img className="img" src="https://logodix.com/logo/1617208.jpg" alt="" />
+            <label htmlFor="file">
+            </label>
+            <input type="file" id="file" style={{ display: "none" }} />
+          </div>
+          <button className="button">Update</button>
+        </div>
+      </form>
+
+    </div>
+  </div>
+</div>
+);
+}
