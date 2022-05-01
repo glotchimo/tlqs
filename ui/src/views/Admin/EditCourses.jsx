bdef62e0a5ab09e5512d2215f08a035f13d93bc7
hewrtarkhany <hewrtarkhany@gmail.com>
2022-05-01 14:29:18 -0700

    edit course completed

--- ui/src/views/Admin/EditCourses.jsx
+++ ui/src/views/Admin/EditCourses.jsx
@@ -1,76 +1,44 @@
-
-import * as React from 'react';
-import React, { useState } from "react";
-import { DataGrid} from '@mui/x-data-grid';
-import Button from '@mui/material/Button';
-import React,{ useState } from "react";
-
-
-export default ()=> {
-
-  const columns = [
-    { field: 'title', headerName: 'Title', width: 130 },
-    { field: 'department', headerName: 'Department', width: 130 },
-    { field: 'code', headerName: 'Code', width: 130 },
-  ];
-  
-  const [inputs ,setInputs]= React.useState();
-
-  const apiPost = () => {
-    fetch("http://localhost:8080/courses/", {
-      method: "POST",
-      body: JSON.stringify({
-        title: inputs.title,
-        department: inputs.department,
-        code: inputs.code,
-      }),
-      headers: {
-        "Content-type":  "application/json; charset-UTF-8",
-        'Accept': 'application/json' },})
-
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-
-  };
-  const handleChange = (event) => {
-    setInputs(()=> ({
-      ...inputs, 
-        [event.target.name]: event.target.value,
-       
-    }))
-  }
+import React from "react";
+import { useState } from "react";
+const EditCourses = () => {
+  const [title, setTitle] = useState('');
+  const [departmet, setDepartment] = useState('');
+  const [code, setCode] = useState('');
   const handleSubmit = (event) => {
-    apiPost();
-    handleChange();
-    console.log("hadnle submit",inputs);
-   
+    event.preventDefault();
+    const courseData = { title,departmet,code };
+
+    fetch('http://localhost:8080/courses/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(courseData)
+    }).then(() => {
+      console.log('new  course updated', courseData);
+    })
   }
-
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      
-      
-      <DataGrid
-       rows={inputs}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-      <Button
-  onClick={() => {
-    handleSubmit();
-  }}
->
-  update
-</Button>
+    <div style={{flex: '4',padding: '20px'}}>
+    <div style={{ display: 'flex',alignitems: 'center',justifycontent: 'space-between'}}>
+      <h1 style={{ color: 'crimson'}}>Edit Course</h1>
     </div>
-  );}
-
-
-
-
-
-
-
-
+    <div style={{display: 'flex',margintop: '20px'}}>
+      <div style={{flex: '2',padding: '20px',webkitBoxShadow: '0px 0px 15px -10px rgba(0, 0, 0, 0.75)',boxShadow: '0px 0px 15px -10px rgba(0, 0, 0, 0.75)',marginLeft: '20px'}}>
+        <form onSubmit={handleSubmit}  style={{display: 'flex',justifyContent: 'space-between',marginTop: '20px'}}> 
+            <div style={{display: 'flex',flexDirection: 'column', marginTop: '10px'}}>
+              <label>Title:</label>
+              <input style={{border: 'none',width: '250px',height: '30px',borderBottom: '1px solid gray'}} type="text" required value={title} onChange={(event) => setTitle(event.target.value)}/>
+              <label>Department:</label>
+              <input  style={{border: 'none',width: '250px',height: '30px',borderBottom: '1px solid gray'}} type="number" required value={departmet} onChange={(event) => setDepartment(event.target.value)}/>
+              <label>Code:</label>
+              <input  style={{border: 'none',width: '250px',height: '30px',borderBottom: '1px solid gray'}} type="text" required value={code} onChange={(event) => setCode(event.target.value)}/>
+              <button>Update</button>  
+            </div>       
+        </form>
+      </div>
+    </div>
+  </div>
+    
+  );
+}
+ 
+export default EditCourses;
