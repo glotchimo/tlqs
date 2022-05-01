0473389333e2977db31c752a75daf9ec663b1147
Elliott Maguire <glotchimo@pm.me>
2022-04-30 21:39:05 -0700

    remove many2many fields from user

--- user.go
+++ user.go
@@ -16,12 +16,10 @@ const (
 )
 
 type User struct {
-	ID       string    `gorm:"primaryKey" json:"id"`
-	Name     string    `json:"name"`
-	Email    string    `json:"email"`
-	Role     Role      `json:"role"`
-	Sessions []Session `gorm:"many2many:user_sessions" json:"sessions"`
-	Courses  []Course  `gorm:"many2many:user_courses" json:"courses"`
+	ID    string `gorm:"primaryKey" json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Role  Role   `json:"role"`
 }
 
 func (user *User) BeforeCreate(scope *gorm.DB) error {
