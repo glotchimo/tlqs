7e06a8561afb6de0cba9aa80efc96f7233f43364
hewrtarkhany <hewrtarkhany@gmail.com>
2022-05-01 18:45:56 -0700

    admin with appbar and deleting stuff from course

--- ui/src/views/Admin/Admin.jsx
+++ ui/src/views/Admin/Admin.jsx
@@ -1,5 +1,25 @@
 import * as React from "react";
-
-export default () => {
-  return "Admin";
- };
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+export default function Admin() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Button color="error" href="https://www.google.com/">
+              Users
+            </Button>
+            <Button color="error">Courses</Button>
+            <Button color="error">Sessions</Button>
+            <Button color="error">Topics</Button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    
+  );
+}
