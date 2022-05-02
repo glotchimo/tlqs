1f1322b0df37802f848339b9bf8e2fb7410df6d3
hewrtarkhany <hewrtarkhany@gmail.com>
2022-05-01 18:45:56 -0700

    admin with appbar and deleting stuff from course

--- ui/src/views/Admin/Courses.jsx
+++ ui/src/views/Admin/Courses.jsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import React, { useState, useEffect } from "react";
 import { DataGrid} from '@mui/x-data-grid';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import React,{ useState } from "react";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditIcon from '@mui/icons-material/Edit';
@@ -57,20 +51,7 @@ export default ()=> {
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <div style={{ display: "flex", alignItems: "baseline"}}>
-           <Typography variant="title" color="inherit" noWrap>
-             COURSE
-           </Typography>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </Box>
+
       <DataGrid
        rows={rows}
         columns={columns}
