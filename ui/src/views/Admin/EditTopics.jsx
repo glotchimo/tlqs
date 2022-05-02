28626d1455d5d49000826a0d8b50e38d3c0fa75d
hewrtarkhany <hewrtarkhany@gmail.com>
2022-05-01 19:15:32 -0700

    edittopics finished

--- ui/src/views/Admin/EditTopics.jsx
+++ ui/src/views/Admin/EditTopics.jsx
@@ -1,42 +1,41 @@
-import "./edittopics.css";
-import * as React from 'react';
-export default  function EditTopics() {
+import React from "react";
+import { useState } from "react";
+const EditTopics = () => {
+  const [name, setName] = useState('');
+  const [courseid, setCourseID] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const courseData = { name,courseid };
 
-return (
-<div className="topic">
-  <div className="titleContainer">
-    <h1 className="title">Edit Topics</h1>
-  </div>
-  <div className="container">
-    <div className="update">
-      <form className="form">
-        <div className="left">
-
-          <div className="item">
-
-            <label>Name</label>
-            <input type="text" placeholder="name" className="input" />
-          </div>
-
-          <div className="item">
-            <label>CourseID</label>
-            <input type="text" placeholder="courseid" className="input" />
-          </div>
-
-        </div>
-        <div className="right">
-          <div className="upload">
-            <img className="img" src="https://logodix.com/logo/1617208.jpg" alt="" />
-            <label htmlFor="file">
-            </label>
-            <input type="file" id="file" style={{ display: "none" }} />
-          </div>
-          <button className="button">Update</button>
-        </div>
-      </form>
-
+    fetch('http://localhost:8080/courses/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(courseData)
+    }).then(() => {
+      console.log('new  course updated', courseData);
+    })
+  }
+  return (
+    <div style={{flex: '4',padding: '20px'}}>
+    <div style={{ display: 'flex',alignitems: 'center',justifycontent: 'space-between'}}>
+      <h1 style={{ color: 'crimson'}}>Edit Course</h1>
+    </div>
+    <div style={{display: 'flex',margintop: '20px'}}>
+      <div style={{flex: '2',padding: '20px',webkitBoxShadow: '0px 0px 15px -10px rgba(0, 0, 0, 0.75)',boxShadow: '0px 0px 15px -10px rgba(0, 0, 0, 0.75)',marginLeft: '20px'}}>
+        <form onSubmit={handleSubmit}  style={{display: 'flex',justifyContent: 'space-between',marginTop: '20px'}}> 
+            <div style={{display: 'flex',flexDirection: 'column', marginTop: '10px'}}>
+              <label>Name:</label>
+              <input style={{border: 'none',width: '250px',height: '30px',borderBottom: '1px solid gray'}} type="text" required value={name} onChange={(event) => setName(event.target.value)}/>
+              <label>CourseID:</label>
+              <input  style={{border: 'none',width: '250px',height: '30px',borderBottom: '1px solid gray'}} type="text" required value={courseid} onChange={(event) => setCourseID(event.target.value)}/>
+              <button>Update</button>  
+            </div>       
+        </form>
+      </div>
     </div>
   </div>
-</div>
-);
+    
+  );
 }
+ 
+export default EditTopics;
