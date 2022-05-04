56e95dd134f0c92c502473c8f42c815aa07005cf
hewrtarkhany <hewrtarkhany@gmail.com>
2022-05-03 18:25:22 -0700

    changing the session buttons

--- ui/src/views/Admin/Sessions.jsx
+++ ui/src/views/Admin/Sessions.jsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import React, { useState, useEffect } from "react";
 import { DataGrid} from '@mui/x-data-grid';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditIcon from '@mui/icons-material/Edit';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -28,15 +22,14 @@ export default ()=> {
     {field: 'edit', headerName: 'Edit', width: 100,
     renderCell: (params) => {
       return (
-        // a dom LINK here in a tag to link it to apage where user can edit
-            <> <EditIcon /></>
+           <EditIcon />
         )},
     },
     {field: 'reate', headerName: 'Create', width: 100,
     renderCell: (params) => {
       return (
-        // a dom here to link to a page like <Link to {page we want}<Link> to create a new user
-        <> <AddCircleIcon /></>)},
+         <AddCircleIcon />
+        )},
     },
   ];
 
@@ -59,21 +52,8 @@ export default ()=> {
   };
   
     return (  
-      <div style={{ height: 400, width: '100%' }}>
-          <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <div style={{ display: "flex", alignItems: "baseline"}}>
-           <Typography variant="title" color="inherit" noWrap>
-             SESSIONS
-           </Typography>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <div style={{ flex:'4',padding: '20px'}}>
+      
         <DataGrid
          rows={rows}
           columns={columns}
