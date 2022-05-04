2aa1ef10a6b4d0ff6f145fba92e14a107bf92422
Elliott Maguire <glotchimo@pm.me>
2022-05-03 20:26:03 -0700

    fix up admin view

--- ui/src/views/Admin/Admin.jsx
+++ ui/src/views/Admin/Admin.jsx
@@ -4,23 +4,54 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Courses from "./Courses";
+import Sessions from "./Sessions";
+import Users from "./Users";
 
-export default function Admin() {
-  return (
+export default () => {
+  const [view, setView] = React.useState(0);
+
+  let bar = (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Button href="Users.jsx">
+            <Button style={{ color: "white" }} onClick={() => setView(0)}>
+              Courses
+            </Button>
+            <Button style={{ color: "white" }} onClick={() => setView(1)}>
+              Sessions
+            </Button>
+            <Button style={{ color: "white" }} onClick={() => setView(2)}>
               Users
             </Button>
-            <Button href="Courses.jsx">Courses</Button>
-            <Button href="Sessions.jsx">Sessions</Button>
-            <Button href="Topics.jsx">Topics</Button>
           </Typography>
         </Toolbar>
       </AppBar>
     </Box>
-    
   );
-}
+
+  switch (view) {
+    case 0:
+      return (
+        <>
+          {bar}
+          <Courses />
+        </>
+      );
+    case 1:
+      return (
+        <>
+          {bar}
+          <Sessions />
+        </>
+      );
+    default:
+      return (
+        <>
+          {bar}
+          <Users />
+        </>
+      );
+  }
+};
