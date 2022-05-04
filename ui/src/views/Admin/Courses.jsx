373b3ba0161c3cda167df4c29b9b311b04ab5e89
hewrtarkhany <hewrtarkhany@gmail.com>
2022-05-03 18:21:06 -0700

    course changes

--- ui/src/views/Admin/Courses.jsx
+++ ui/src/views/Admin/Courses.jsx
@@ -1,53 +1,45 @@
 import * as React from 'react';
 import React, { useState, useEffect } from "react";
-import { DataGrid} from '@mui/x-data-grid';
-import React,{ useState } from "react";
+import { DataGrid } from '@mui/x-data-grid';
+import React, { useState } from "react";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditIcon from '@mui/icons-material/Edit';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import Button from '@mui/material/Button';
 
-
-export default ()=> {
+export default () => {
   const columns = [
     { field: 'id', headerName: 'ID', width: 130 },
     { field: 'title', headerName: 'Ttile', width: 130 },
     { field: 'code', headerName: 'Code', width: 130 },
-    {field: 'delete', headerName: 'Delete', width: 100,
-    renderCell: (params) => {
-      return (
-        <DeleteOutlineIcon onClick={() => handleDelete(params.row.id)}/>)},},
-  
-    {field: 'edit', headerName: 'Edit', width: 100,
-    renderCell: (params) => {
-      return (
-        <Button
-        variant="line"
-        color="error"
-        a
-        href="/course/"
-      >
-        <EditIcon />
-      </Button>
-        )},
+    {
+      field: 'delete', headerName: 'Delete', width: 100,
+      renderCell: (params) => {
+        return (
+          <DeleteOutlineIcon onClick={() => handleDelete(params.row.id)} />)
+      },
     },
-    {field: 'reate', headerName: 'Create', width: 100,
-    renderCell: (params) => {
-      return (
 
-        <Button
-        variant="line"
-        color="error"
-        a
-        href="/EditCourse/"
-      >
+    {
+      field: 'edit', headerName: 'Edit', width: 100,
+      renderCell: (params) => {
+        return (
+
+          <EditIcon />
+        )
+      },
+    },
+    {
+      field: 'reate', headerName: 'Create', width: 100,
+      renderCell: (params) => {
+        return (
+
           <AddCircleIcon />
 
-      </Button>
 
-         )},
+        )
+      },
     },];
-  
+
   const [rows, setRows] = useState([]);
 
   const apiGet = () => {
@@ -55,9 +47,11 @@ export default ()=> {
       .then((response) => response.json())
       .then((json) => {
         setRows(json);
-          json.map((course)=>{
-            rows.push({title:course.title, code:course.code},);
-          })}); };
+        json.map((course) => {
+          rows.push({ title: course.title, code: course.code },);
+        })
+      });
+  };
 
   useEffect(() => {
     apiGet();
@@ -71,11 +65,12 @@ export default ()=> {
     <div style={{ height: 400, width: '100%' }}>
 
       <DataGrid
-       rows={rows}
+        rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
       />
     </div>
-  );}
+  );
+}
