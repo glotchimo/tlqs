a1e4a075c86aca6e5fd188889f3c37b61136a5c6
hewrtarkhany <hewrtarkhany@gmail.com>
2022-06-01 13:36:26 -0700

    changing the user to patch

--- ui/src/views/Admin/Users.jsx
+++ ui/src/views/Admin/Users.jsx
@@ -7,15 +7,17 @@ import {
   GridToolbarFilterButton,
   GridToolbarDensitySelector,
   GridToolbarExport,
+
 } from "@mui/x-data-grid";
 
 export default () => {
   const [rows, setRows] = useState([]);
+
   const columns = [
     { field: "id", headerName: "ID", flex: 1 },
     { field: "name", headerName: "Names", flex: 1 },
     { field: "email", headerName: "Email", flex: 1 },
-    { field: "role", headerName: "Role", flex: 1 },
+    { field: "role", headerName: "Role", flex: 1, editable:true },
     { field: "sessions", headerName: "Sessions", flex: 1 },
     { field: "courses", headerName: "Courses", flex: 1 },
   ];
@@ -27,6 +29,7 @@ export default () => {
         <GridToolbarFilterButton style={{color:"#b7142e"}} />
         <GridToolbarDensitySelector style={{color:"#b7142e"}} />
         <GridToolbarExport style={{color:"#b7142e"}}/>
+        
       </GridToolbarContainer>
     );
   };
@@ -38,15 +41,39 @@ export default () => {
         setRows(json);
       });
   };
+  const patchUsers=()=>{
+   requestOptions = {
+    method: 'POST', 
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title: 'post react' })
+};
+fetch("/users", requestOptions)
+    .then(async response => {
+        const isJson = response.headers.get('content-type')?.includes('application/json');
+        const data = isJson && await response.json();
+
+        if (!response.ok) {
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+
+        this.setState({ postId: data.id })
+    })
+    .catch(error => {
+        //this.setState({ errorMessage: error.toString() });
+        console('There was an error!');
+    });
+  }
 
   useEffect(() => {
     getUsers();
+    patchUsers();
   }, []);
 
   return (
-    <DataGrid
+    <DataGrid 
       rows={rows}
-      columns={columns}
+      columns={columns[{ field: 'role', editable: true }]}
       pageSize={100}
       rowsPerPageOptions={[100]}
       autoHeight
