229fb4aa48088ed317d60633cf7f59fc54f88e6e
TechGlue <garcialuis24@hotmail.com>
2022-06-04 13:56:59 -0700

    changed from boolean to completed and not completed

--- ui/src/views/Admin/Sessions.jsx
+++ ui/src/views/Admin/Sessions.jsx
@@ -19,16 +19,16 @@ export default () => {
     { field: "topic", headerName: "Topic", flex: 1 },
     { field: "description", headerName: "Description", flex: 1 },
     { field: "retrospective", headerName: "Retrospective", flex: 1 },
-    {field: "completed", headerName: "Status", flex: 1},
+    { field: "completed", headerName: "Status", flex: 1 },
   ];
 
   const toolbar = () => {
     return (
       <GridToolbarContainer>
-        <GridToolbarColumnsButton style={{color:"#b7142e"}} />
-        <GridToolbarFilterButton  style={{color:"#b7142e"}}/>
-        <GridToolbarDensitySelector  style={{color:"#b7142e"}}/>
-        <GridToolbarExport style={{color:"#b7142e"}} />
+        <GridToolbarColumnsButton style={{ color: "#b7142e" }} />
+        <GridToolbarFilterButton style={{ color: "#b7142e" }} />
+        <GridToolbarDensitySelector style={{ color: "#b7142e" }} />
+        <GridToolbarExport style={{ color: "#b7142e" }} />
       </GridToolbarContainer>
     );
   };
@@ -37,7 +37,20 @@ export default () => {
     fetch("/sessions/")
       .then((response) => response.json())
       .then((json) => {
-        setRows(json);
+        const data = json.map((session) => {
+          return {
+            id: session.id,
+            student: session.student,
+            tutor: session.tutor,
+            course: session.course,
+            topic: session.topic,
+            description: session.description,
+            retrospective: session.retrospective,
+            completed: session.completed ? "Completed" : "Not completed",
+          };
+        });
+
+        setRows(data);
       });
   };
 
@@ -53,7 +66,7 @@ export default () => {
       rowsPerPageOptions={[100]}
       autoHeight
       components={{ Toolbar: toolbar }}
-      style={{ marginTop: "0.5rem"}}
+      style={{ marginTop: "0.5rem" }}
     />
   );
 };
